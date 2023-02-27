b7f611040ee2333cf88afcf32661e548f75426ba
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-02-27 12:11:24 +0200

    хедер + поиск + тема

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createMainPage } from './js/sendrequest';
-
+import { mobileMenu } from './js/mobileMenu';
 const inputSearch = document.getElementById('searchForm');
 
 inputSearch.addEventListener('submit', createMainPage);
