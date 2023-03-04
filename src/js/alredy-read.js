af4eb26d00a322667ad6a8c7899e0b672a61a393
Dmytro Potapenko <dima.lmkh@gmail.com>
2023-03-04 15:09:00 +0200

    fix

--- src/js/alredy-read.js
+++ src/js/alredy-read.js
@@ -1,20 +1,22 @@
-export default function onReadCard(e){
+export function onReadCard(e) {
   let card = null;
- const today = new Date();
- const now = today.toLocaleDateString('en-GB');
-   
- if (e.target.closest('.news-card__read-more')){
-  
-  card = e.target.closest('.news-card')
-   
-   if (card){  
-     card.classList.add('reading_card')
-   }
+  const today = new Date();
+  const now = today.toLocaleDateString('en-GB');
 
-   if (arr.includes(`${card.id}`)){
-    return}
-    else{arr.push(`${card.id}`)}
+  if (e.target.closest('.news-card__read-more')) {
+    card = e.target.closest('.news-card');
+
+    if (card) {
+      card.classList.add('reading_card');
+    }
+
+    if (arr.includes(`${card.id}`)) {
+      return;
+    } else {
+      arr.push(`${card.id}`);
+    }
 
     //  console.log(arr)
-   localStorage.setItem(`${now}`, JSON.stringify(arr));}
+    localStorage.setItem(`${now}`, JSON.stringify(arr));
   }
+}
